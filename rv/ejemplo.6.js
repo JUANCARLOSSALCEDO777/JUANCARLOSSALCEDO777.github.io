325721ad4bcaf1cf14c34d1f747ca247eaa21aa7
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2016-03-16 10:30:48 -0600

    Update ejemplo.6.js

--- rv/ejemplo.6.js
+++ rv/ejemplo.6.js
@@ -1,6 +1,6 @@
 funtion setup(){
-THREE.ImageUtils.crossOrigin =' '
-var textura =THREE.ImageUtils.loadTexture('http//three.js.org/examples/textures/crate.gif')
+THREE.ImageUtils.crossOrigin ='';
+var textura =THREE.ImageUtils.loadTexture('http//three.js.org/examples/textures/crate.gif');
 var material=new THREE.MeshBasicMaterial({map: textura});
 var forma= new THREE.BoxGeometry(1,1,1); 
 malla = new THREE.Mesh (forma,material);
