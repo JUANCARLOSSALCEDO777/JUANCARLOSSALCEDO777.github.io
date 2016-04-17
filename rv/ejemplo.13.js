a8d172dee206c4668c2be139152da1b5720ea709
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2016-04-17 14:43:12 -0500

    Update ejemplo.13.js

--- rv/ejemplo.13.js
+++ rv/ejemplo.13.js
@@ -13,9 +13,9 @@ escena.add(caja1);
 escena.add(caja2);
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z=-31;
+camara.position.z=0;
 camara.position.x=0;
-camara.position.y=0;
+camara.position.y=-31;
 camara.lookAt( escena.position );
 renderer = new THREE.WebGLRenderer();
 
