dbcbd4310e034fa78671285661f2c8961c664bb0
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2016-04-17 23:32:29 -0500

    Update ejemplo.13.js

--- rv/ejemplo.13.js
+++ rv/ejemplo.13.js
@@ -12,7 +12,7 @@ var llantaext  = new THREE.CylinderGeometry( 1, 1, 2, 32,1,false,0,(Math.PI) );
 
 llantaext1= new THREE.Mesh (llantaext,materialllanta);
 llantaext1.position.x=7;
-llantaext1.position.y=3;
+llantaext1.position.y=21;
 llantaext1.position.z=0;
 llantaext1.rotation.z +=Math.PI/2;
 
