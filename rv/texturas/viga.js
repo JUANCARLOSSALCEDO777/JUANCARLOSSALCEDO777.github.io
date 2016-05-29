59ed160c7615d999164415056b7825367f1c91a8
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2016-05-29 18:54:33 -0500

    Add files via upload

--- rv/texturas/viga.js
+++ rv/texturas/viga.js
@@ -9,7 +9,7 @@
 		"normals"       : 84,
 		"colors"        : 0,
 		"uvs"           : [],
-		"materials"     : 2,
+		"materials"     : 1,
 		"morphTargets"  : 0,
 		"bones"         : 0
 	},
@@ -19,34 +19,7 @@
 	"materials" : [	{
 		"DbgColor" : 15658734,
 		"DbgIndex" : 0,
-		"DbgName" : "Clear Anodized Aluminum",
-		"blending" : "NormalBlending",
-		"colorAmbient" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
-		"colorDiffuse" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
-		"colorSpecular" : [0.5, 0.5, 0.5],
-		"depthTest" : true,
-		"depthWrite" : true,
-		"shading" : "Lambert",
-		"specularCoef" : 50,
-		"transparency" : 1.0,
-		"transparent" : false,
-		"vertexColors" : false
-	},
-
-	{
-		"DbgColor" : 15597568,
-		"DbgIndex" : 1,
-		"DbgName" : "Black Anodized Aluminum",
-		"blending" : "NormalBlending",
-		"colorAmbient" : [0.0, 0.0, 0.0],
-		"colorDiffuse" : [0.0, 0.0, 0.0],
-		"colorSpecular" : [0.5, 0.5, 0.5],
-		"depthTest" : true,
-		"depthWrite" : true,
-		"shading" : "Lambert",
-		"specularCoef" : 50,
-		"transparency" : 1.0,
-		"transparent" : false,
+		"DbgName" : "default",
 		"vertexColors" : false
 	}],
 
