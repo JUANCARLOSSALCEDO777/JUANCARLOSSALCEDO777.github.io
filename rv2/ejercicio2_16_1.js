6aa1083572ab2a3a105b0ac2bcd4ced7a4f48898
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2017-02-16 14:07:29 -0600

    Create ejercicio2_16_1.js

---
+++ rv2/ejercicio2_16_1.js
@@ -0,0 +1,11 @@
+var miCanvas1 = document.getElementById("miCanvas1");
+var contexto1 = miCanvas.getContext("2d");
+contexto1.fillStyle="red";
+contexto1.fillRect(30, 30, 50, 50);
+
+var miCanvas2 = document.getElementById("miCanvas2");
+var contexto2 = miCanvas.getContext("2d");
+contexto1.fillStyle="blue";
+contexto1.fillRect(30, 30, 50, 50);
+contexto1.fillStyle="yellow";
+contexto1.fillRect(50, 50, 70, 70);
