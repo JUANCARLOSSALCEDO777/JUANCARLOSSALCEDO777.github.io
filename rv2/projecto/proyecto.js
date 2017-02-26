08ef6b145eb6175c2e603cdfa078eebba4b6f3e7
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2017-02-25 20:54:44 -0600

    Update proyecto.js

--- rv2/projecto/proyecto.js
+++ rv2/projecto/proyecto.js
@@ -2,7 +2,7 @@ var escena = new THREE.Scene();
 escena.add( mallaV1 );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position = new THREE.Vector3( 10, 10, 10 ); 
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, 
