aac6c7df0839a69985ecd3981c06d7596733d5bd
juan carlos salcedo <correojuancarlos2014@gmail.com>
2017-02-26 22:08:01 -0600

    orientando las caras
    la deficinicion de la normales de las caras depende del orden de como se
    colocan los vertices de las mismas (regla de la mano derecha ) solo la
    cara es visible de su lado donde apunta su normal

--- rv2/projecto/FigVertices.js
+++ rv2/projecto/FigVertices.js
@@ -1,24 +1,26 @@
 var formaV = new THREE.Geometry();
 
-formaV.vertices.push( new THREE.Vector3( 1,  0,  0 ) ); // Vértice 0
-formaV.vertices.push( new THREE.Vector3( -1,  0, 0 ) ); // Vértice 1
-formaV.vertices.push( new THREE.Vector3(0,  1, 0 ) );   // Vértice 2
-formaV.vertices.push( new THREE.Vector3(0,  -1,  0 ) ); // Vértice 3
-formaV.vertices.push( new THREE.Vector3( 0,  0,  1 ) ); // Vértice 4
+formaV.vertices.push(new THREE.Vector3( 1,  0,  0 ) ); // Vértice 0
+formaV.vertices.push(new THREE.Vector3( -1,  0, 0 ) ); // Vértice 1
+formaV.vertices.push(new THREE.Vector3(0,  1, 0 ) );   // Vértice 2
+formaV.vertices.push(new THREE.Vector3(0,  -1,  0 ) ); // Vértice 3
+formaV.vertices.push(new THREE.Vector3( 0,  0,  1 ) ); // Vértice 4
 
-formaV.faces.push( new THREE.Face3( 0, 1, 2 ) ); // Cara 0
-formaV.faces.push( new THREE.Face3( 0, 1, 3 ) ); // Cara 1
-formaV.faces.push( new THREE.Face3( 0, 2, 4 ) ); // Cara 2
-formaV.faces.push( new THREE.Face3( 0, 3, 4 ) ); // Cara 3
-formaV.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
-formaV.faces.push( new THREE.Face3( 1, 3, 4 ) ); // Cara 5
+formaV.faces.push(new THREE.Face3( 0, 1, 2 ) ); // Cara 0
+formaV.faces.push(new THREE.Face3( 1, 0, 3 ) ); // Cara 1
+formaV.faces.push(new THREE.Face3( 0, 2, 4 ) ); // Cara 2
+formaV.faces.push(new THREE.Face3( 3, 0, 4 ) ); // Cara 3
+formaV.faces.push(new THREE.Face3( 2, 1, 4 ) ); // Cara 4
+formaV.faces.push(new THREE.Face3( 1, 3, 4 ) ); // Cara 5
 
 formaV.computeBoundingSphere();
 formaV.computeFaceNormals();
 
-var materialV1 = new THREE.MeshNormalMaterial({color: 0x4A2FCE});
-var materialV2 = new THREE.MeshNormalMaterial({color: 0xE41B1B});
+var materialV1 = new THREE.MeshNormalMaterial();
+var materialV2 = new THREE.MeshNormalMaterial();
 
 var mallaV1 = new THREE.Mesh( formaV, materialV1 );
 var mallaV2 = new THREE.Mesh( formaV, materialV2 );
+//mallaV1.rotateX(Math.PI/4);
+//mallaV2.rotateX(Math.PI/4);
 
--- rv2/projecto/proyecto.js
+++ rv2/projecto/proyecto.js
@@ -1,16 +1,29 @@
 var escena = new THREE.Scene();
-escena.add( mallaV1 );
-
+    
+//ejes
+    ejes = new THREE.AxisHelper( 50 );
+    ejes.position.set( 0, 0, 0 );
+    escena.add( ejes );
+//ejes
+//camara
 var camara = new THREE.PerspectiveCamera();
 
-camara.position =new THREE.Vector3( 15, 15, 15 );
+camara.position.set(5,5,5);
+//camara.position.z=-5; 
 camara.lookAt( escena.position );
+//camara
+escena.add( mallaV1 );
+
+    
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, 
                       window.innerHeight*.95 );
                       
-                      
+  
+
                       
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
+
+
