c19fd1aa85a9a481fb86b48cbca8f05fab9a3e58
JUANCARLOSSALCEDO777 <correojuancarlos2014@gmail.com>
2017-05-09 13:52:50 -0500

    Update textura2.js

--- rv2/textura2.js
+++ rv2/textura2.js
@@ -16,7 +16,7 @@ escena = new THREE.Scene();
 escena.add(malla);
 escena.add(luzPuntual);
 camara = new THREE.PerspectiveCamera();
-camara.position.z=40;
+camara.position.z=10;
 renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
